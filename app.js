fde16238b3cc28836696c4dc15e3375be81d7a14
Sasidharan-tech <153338800+Sasidharan-tech@users.noreply.github.com>
2026-03-25 19:09:06 +0530

    Fix welcome message in root route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcom to ShopEasy!');
+  res.send('Welcome to ShopEasy by Sasidharan!');
 });
 
 app.listen(port, () => {
